f728f735e9513d10fb7c1d0a249e5b05c26a02b5
Matthieu Lamerant <matthieu.lamerant-isc.France@ibm.com>
2017-08-08 16:58:18 +0200

    Final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { ModalModule } from 'angular2-modal';
+import { BootstrapModalModule } from 'angular2-modal/plugins/bootstrap';
 
 import { AppComponent } from './app.component';
+import { FavoriteListComponent } from './favorite-list/favorite-list.component';
+import { FavoriteService } from './services/favorite.service';
+import { HomeComponent } from './home/home.component';
+import { YoutubeSearchComponent } from './youtube-search/youtube-search.component';
+import { YoutubeService } from './services/youtube.service';
+import { YoutubePlayerModule } from 'ng2-youtube-player';
+
+const ROUTES = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'favorites',
+    component: FavoriteListComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FavoriteListComponent,
+    HomeComponent,
+    YoutubeSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ModalModule.forRoot(),
+    BootstrapModalModule,
+    RouterModule.forRoot(ROUTES),
+    YoutubePlayerModule
   ],
-  providers: [],
+  providers: [ FavoriteService, YoutubeService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
